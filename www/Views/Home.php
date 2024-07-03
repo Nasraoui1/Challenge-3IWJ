7e130e0997c9e3e82feeec2a6bdd7358cd3ec424
Nasraoui1 <mahmoudaziznasraoui@gmail.com>
2024-07-03 05:29:37 +0200

    commit

--- www/Views/Home.php
+++ www/Views/Home.php
@@ -127,6 +127,7 @@ $id_role = $_SESSION['id_role'] ?? null;
             <?php
             if (isset($_SESSION['id_role'])) : ?>
                  <a href="/dashboard" class="btn btn-dashboard">Dashboard</a>;
+            <?php endif; ?>
         </nav>
     </div>
 </header>
