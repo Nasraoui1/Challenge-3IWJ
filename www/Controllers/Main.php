28f0c6001194beac71758a8d8193ac06638435c5
Nasraoui1 <mahmoudaziznasraoui@gmail.com>
2024-07-03 05:07:22 +0200

    commit

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -2,24 +2,17 @@
 namespace App\Controller;
 
 use App\Core\View;
-use App\Core\SQL;
-use App\Models\StatUser;
-use App\Models\User;
-use App\Models\Page;
-use App\Models\Media;
-use App\Models\Comment;
-use App\Models\Project;
-use App\Models\Tag;
-use App\Models\Setting;
-use App\Models\Status;
-use App\Models\Role;
+
 
 
 class Main
 {
     public function home()
     {
-        include dirname(__DIR__) . '/Views/home.php';
+        $id_role = $_GET["id_role"];
+        $view = new View("home");
+        $view->assign("id_role", $id_role);
+        $view->render();
     }
     public function logout()
     {
