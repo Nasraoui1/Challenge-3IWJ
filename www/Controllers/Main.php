a88901de65a7e6c4bf84b170f5b783d30cb83e94
Nasraoui1 <mahmoudaziznasraoui@gmail.com>
2024-07-03 05:15:09 +0200

    commit

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -9,6 +9,10 @@ class Main
 {
     public function home()
     {
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
         $id_role = $_SESSION['id_role'];
         $view = new View("home");
         $view->assign("id_role", $id_role);
